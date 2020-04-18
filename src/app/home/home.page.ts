a10257c25e74b168648421f60c4cf7320ff31186
GustavoNapa <gustavosouzacco@gmail.com>
2020-04-18 15:50:13 -0300

    Cronometro funcionando
    Funcionando com segundos, minutos e horas.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  horas:number = 0;
+  minutos:number = 0;
+  segundos:number = 0;
+
+  interval;
+
+  play:boolean = false;
 
   constructor() {}
+
+  start(){
+    this.play = true;
+
+    console.info("Start");
+    this.interval = setInterval(() => {
+      this.segundos++;
+      if(this.segundos === 60) {
+        this.minutos++;
+        this.segundos = 0;
+        if(this.minutos === 60) {
+          this.horas++;
+          this.minutos = 0;
+        }
+      }
+    }, 1000);
+  }
+
+  pause(){
+    this.play = false;
+    
+    clearInterval(this.interval);
+  }
+
+  zerar(){
+    this.horas = 0;
+    this.minutos = 0;
+    this.segundos = 0;
+  }
 
 }
